98168a63e8c3c6635508bf9e7dc05b4876508f26
唐杜 <tangdu@qccr.com>
2018-01-10 20:40:12 +0800

    Initial commit

---
+++ src/main/java/org/mybatis/generator/internal/TangCommentGenerator.java
@@ -0,0 +1,169 @@
+package org.mybatis.generator.internal;
+
+import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.config.PropertyRegistry;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Set;
+
+import static org.mybatis.generator.internal.util.StringUtility.isTrue;
+
+/**
+ * 添加MySQL注释生成器
+ *
+ * @author tangdu
+ * @version $: TangCommentGenerator.java, v 0.1 2018年01月10日 下午4:38 tangdu Exp $
+ */
+public class TangCommentGenerator implements CommentGenerator {
+    private Properties properties;
+
+    private boolean suppressDate;
+
+    private SimpleDateFormat dateFormat1=new SimpleDateFormat("yyyy年MM月dd日 下午hh:mm");
+    private SimpleDateFormat dateFormat2=new SimpleDateFormat("yyyy年MM月dd日 上午hh:mm");
+
+    public TangCommentGenerator() {
+        super();
+        properties = new Properties();
+        suppressDate = false;
+    }
+
+    @Override
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+    }
+
+    @Override
+    public void addComment(XmlElement xmlElement) {
+    }
+
+    @Override
+    public void addRootComment(XmlElement rootElement) {
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
+
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
+
+    }
+
+    @Override
+    public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+
+    }
+
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+        this.properties.putAll(properties);
+        suppressDate = isTrue(properties
+                .getProperty(PropertyRegistry.COMMENT_GENERATOR_SUPPRESS_DATE));
+    }
+
+
+    @Override
+    public void addClassComment(InnerClass innerClass,
+                                IntrospectedTable introspectedTable) {
+        String username=System.getProperty("user.name");
+        String dateStr="";
+        if(new Date().getHours()>=0 && new Date().getHours()<=12){
+            dateStr=dateFormat1.format(new Date());
+        }else{
+            dateStr=dateFormat2.format(new Date());
+        }
+        StringBuilder sb = new StringBuilder();
+        innerClass.addJavaDocLine("/**");
+        sb.append(" * ");
+        sb.append(introspectedTable.getRemarks());
+        sb.append("(").append(introspectedTable.getFullyQualifiedTable().getIntrospectedTableName()).append(")");
+        sb.append("\n");
+        sb.append(" * ");
+        sb.append("\n");
+        sb.append(" * ");
+        sb.append("@author ").append(username).append("\n");
+        sb.append(" * ").append("@version ")
+                .append(String.format("$: %s.java, v 0.1 %s %s Exp $ ",
+                        introspectedTable.getFullyQualifiedTable().getDomainObjectName(),dateStr,username));
+        innerClass.addJavaDocLine(sb.toString());
+        innerClass.addJavaDocLine(" */"); //$NON-NLS-1$
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass,
+                                IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
+        addClassComment(innerClass,introspectedTable);
+    }
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass,
+                                     IntrospectedTable introspectedTable) {
+        addClassComment(topLevelClass,introspectedTable);
+    }
+
+    @Override
+    public void addEnumComment(InnerEnum innerEnum,
+                               IntrospectedTable introspectedTable) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("/** ");
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append(" **/");
+        innerEnum.addJavaDocLine(sb.toString());
+    }
+
+    @Override
+    public void addFieldComment(Field field,
+                                IntrospectedTable introspectedTable,
+                                IntrospectedColumn introspectedColumn) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("/**");
+        sb.append(introspectedColumn.getRemarks());
+        sb.append("**/");
+        field.addJavaDocLine(sb.toString());
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("/**");
+        sb.append(introspectedTable.getFullyQualifiedTable().getDomainObjectName());
+        sb.append("**/");
+        field.addJavaDocLine(sb.toString());
+    }
+
+    @Override
+    public void addGeneralMethodComment(Method method,
+                                        IntrospectedTable introspectedTable) {
+    }
+
+    @Override
+    public void addGetterComment(Method method,
+                                 IntrospectedTable introspectedTable,
+                                 IntrospectedColumn introspectedColumn) {
+
+    }
+
+    @Override
+    public void addSetterComment(Method method,
+                                 IntrospectedTable introspectedTable,
+                                 IntrospectedColumn introspectedColumn) {
+
+    }
+}
---
+++ src/main/java/org/mybatis/generator/plugins/JSONToStringPlugin.java
@@ -0,0 +1,98 @@
+/**
+ *    Copyright 2006-2018 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.generator.plugins;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.IntrospectedTable.TargetRuntime;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.*;
+
+import java.util.List;
+import java.util.Properties;
+
+import static org.mybatis.generator.internal.util.StringUtility.isTrue;
+
+/**
+ * 添加FastJSON序列化
+ *
+ * @author tangdu
+ * @version $: MySQLCommentGenerator.java, v 0.1 2018年01月10日 下午4:38 tangdu Exp $
+ */
+public class JSONToStringPlugin extends PluginAdapter {
+
+    private boolean useToStringFromRoot;
+
+    @Override
+    public void setProperties(Properties properties) {
+        super.setProperties(properties);
+        useToStringFromRoot = isTrue(properties.getProperty("useToStringFromRoot")); //$NON-NLS-1$
+    }
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass,
+            IntrospectedTable introspectedTable) {
+        generateToString(introspectedTable, topLevelClass);
+        return true;
+    }
+
+    @Override
+    public boolean modelRecordWithBLOBsClassGenerated(
+            TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        generateToString(introspectedTable, topLevelClass);
+        return true;
+    }
+
+    @Override
+    public boolean modelPrimaryKeyClassGenerated(TopLevelClass topLevelClass,
+            IntrospectedTable introspectedTable) {
+        generateToString(introspectedTable, topLevelClass);
+        return true;
+    }
+
+    private void generateToString(IntrospectedTable introspectedTable,
+            TopLevelClass topLevelClass) {
+        topLevelClass.getMethods().clear();
+        topLevelClass.addAnnotation("@Setter");
+        topLevelClass.addAnnotation("@Getter");
+        topLevelClass.addImportedType(new FullyQualifiedJavaType("com.alibaba.fastjson.JSON"));
+        topLevelClass.addImportedType(new FullyQualifiedJavaType("lombok.Getter"));
+        topLevelClass.addImportedType(new FullyQualifiedJavaType("lombok.Setter"));
+        Method method = new Method();
+        method.setVisibility(JavaVisibility.PUBLIC);
+        method.setReturnType(FullyQualifiedJavaType.getStringInstance());
+        method.setName("toString"); //$NON-NLS-1$
+        if (introspectedTable.isJava5Targeted()) {
+            method.addAnnotation("@Override"); //$NON-NLS-1$
+        }
+
+        if (introspectedTable.getTargetRuntime() == TargetRuntime.MYBATIS3_DSQL) {
+            context.getCommentGenerator().addGeneralMethodAnnotation(method,
+                    introspectedTable, topLevelClass.getImportedTypes());
+        } else {
+            context.getCommentGenerator().addGeneralMethodComment(method,
+                    introspectedTable);
+        }
+
+         method.addBodyLine("return JSON.toJSONString(this);"); //$NON-NLS-1$
+
+        topLevelClass.addMethod(method);
+    }
+}
